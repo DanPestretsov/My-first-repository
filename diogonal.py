db78c3942ec22b3cfb58ed67fcc66ddab77d3eed
dan <dan.pestrecov2012@gmail.com>
2023-12-08 19:48:44 +0400

    решина задача по заполнению диогонали матрици

--- diogonal.py
+++ diogonal.py
@@ -1,13 +1,12 @@
- #Дано число n и m. Нужно создать список размера n, где под индексом m находится 0, после нуля по возрастанию идут целые числа, а до нуля идут числа по убыванию
+n = int(input())
 
-n = 6 # int(input())
-m = 3 # int(input())
+for m in range(0,n):
 
-# spisock=[3, 2, 1, 0, 1, 2]
-spisock = ["_" for i in range(n)]
-for i in range(m,n):
+  spisock = ["_" for i in range(n)]
+  for i in range(m,n):
     spisock[i]= i - m
 
-for i in range(m):
-  spisock[i]= m - i
-print(spisock)
+  for i in range(m):
+    spisock[i]= m - i
+
+  print(*spisock)
