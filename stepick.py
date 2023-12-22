e943cdc29ee5b50c259c13640a11743d95ba891e
dan <dan.pestrecov2012@gmail.com>
2023-12-22 16:33:18 +0400

    решина задача по cтепик из цикл for задача номер 4

--- stepick.py
+++ stepick.py
@@ -1,8 +1,3 @@
-for i in range(6):
-    print('A' * 3)
-for i in range(5):
-    print('B' * 4)
-print('E')
-for i in range(9):
-    print('T' * 5)
-print('G')
+n = int(input())
+for i in range(n):
+    print('*' * 19)
